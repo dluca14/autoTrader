4e4a0dd3845317dcc3530ebf6668716483bf4140
radu petru <radu.petru@live.com>
2021-05-31 15:53:33 +0300

    -improvements to accounts app -Additional components to frontend

--- auto_trader/settings.py
+++ auto_trader/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'frontend.apps.FrontendConfig'
 ]
 
---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class LoginPage extends Component{
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return <p>This is the login page</p>
+    }
+}
