dbcde9e4898f7d8cb4a8c1685c1427cb7b0603f6
radu petru <radu.petru@live.com>
2021-06-07 15:03:17 +0300

    -market data grabber from cryptocompare.com

--- backend/marketData.py
+++ backend/marketData.py
@@ -2,25 +2,34 @@ import pandas as pd
 import requests
 from typing import List
 from datetime import date
+import json
 
 class marketData(object):
+    endpoints = {'day': 'https://min-api.cryptocompare.com/data/histoday',
+                 'min': 'https://min-api.cryptocompare.com/data/histominute',
+                 'hour': 'https://min-api.cryptocompare.com/data/histohour'}
+
     def __init__(self, coins: List):
-        coins = coins
+        self.coins = coins
 
     def retrieve_data(self) -> None:
-        endpoint_day = 'https://min-api.cryptocompare.com/data/histoday'
-        endpoint_min = 'https://min-api.cryptocompare.com/data/histominute'
-        endpoint_hour = 'https://min-api.cryptocompare.com/data/histohour'
-
         for coin in self.coins:
-            res = requests.get(endpoint_min + '?fsym=' + coin + '&tsym=USD&limit=2000')
-            hist = pd.DataFrame(json.loads(res.content)['Data'])
-            hist = hist.set_index('time')
-            hist.index = pd.to_datetime(hist.index, unit='s')
-            target_col = 'close'
+            for period in self.endpoints.keys():
+                res = requests.get(self.endpoints[period] + '?fsym=' + coin + '&tsym=USD&limit=2000')
+                hist = pd.DataFrame(json.loads(res.content)['Data'])
+                hist = hist.set_index('time')
+                hist.index = pd.to_datetime(hist.index, unit='s')
+                target_col = 'close'
 
-            today = date.today()
-            cur_date = today.strftime("%b-%d-%Y")
+                today = date.today()
+                cur_date = today.strftime("%b-%d-%Y")
 
-            hist.to_csv(cur_date + '_' + coin + '_min.csv')
+                hist.to_csv(cur_date + '_' + coin + period +'.csv')
 
+
+if __name__ == '__main__':
+    coins = ['ADA', 'BTC', 'DOGE', 'ETC', 'ETH', 'GRT', 'MLN', 'REP', 'XLM', 'XRP']
+
+    market = marketData(coins=coins)
+
+    market.retrieve_data()
