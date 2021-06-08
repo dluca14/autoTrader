3065b8e6cde934fa19623f447d056df568fb81ff
petru radu <radu.petru@live.com>
2021-06-08 12:26:04 +0000

    Revert "Merge branch 'integrate-twitter' into 'master'"
    This reverts merge request !6

--- auto_trader/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from auto_trader.models import Tweet
-
-
-class TweetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tweet
-        fields = ['id', 'created', 'user', 'user_location', 'language', 'hashtags', 'text']
