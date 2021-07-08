f918ca1d4caabef1fe4fcac0369fab664d461908
divadluca14 <divadluca14@gmail.com>
2021-07-08 20:18:28 +0300

    Pass arg in url

--- azure/urls.py
+++ azure/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from rest_framework import routers
 
 import azure.views as views
 
+
+# router = routers.SimpleRouter()
+# router.register(r'sentiment_analysis', views.SentimentAnalyzer)
+
 urlpatterns = [
-    path('analyse', views.ListTweets.as_view()),
-    path('analyse/tweet', views.GetMostCommonHashtags.as_view()),
+    path('sentiment_analysis', views.SentimentAnalyzerList.as_view()),
+    path('sentiment_analysis/<tweet_id>/', views.SentimentAnalyzer.as_view()),
+    # path('sentiment_analysis/<int: pk>', views.SentimentAnalyzer.as_view()),
 ]
+# urlpatterns = router.urls
--- azure/views.py
+++ azure/views.py
@@ -1,8 +1,10 @@
 import uuid
 
 import requests
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 import settings_local
 from twitter.models import Tweet
@@ -12,7 +14,36 @@ path = '/text/analytics/v3.0/sentiment'
 constructed_url = settings_local.AZURE['ENDPOINT'] + path
 
 
-class ListTweets(APIView):
+class SentimentAnalyzer(APIView):
+    def get(self, request, tweet_id=None):
+        print('=============', tweet_id)
+        headers = {
+            'Ocp-Apim-Subscription-Key': settings_local.AZURE['SUBSCRIPTION_KEY'],
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+
+        if tweet_id:
+            tweet = Tweet.objects.filter(Tweet.pk == tweet_id)
+        else:
+            tweet = Tweet.objects.all()
+
+        # You can pass more than one object in body.
+        body = {
+            'documents': [
+                {
+                    'language': 'en',
+                    'id': '1',
+                    'text': tweet.text
+                },
+            ]
+        }
+        response = requests.post(constructed_url, headers=headers, json=body)
+
+        return Response(response.json())
+
+
+class SentimentAnalyzerList(APIView):
     """
     * Requires token authentication.
     * Only admin users are able to access this view.
@@ -44,27 +75,3 @@ class ListTweets(APIView):
             final_response.append(response.json())
 
         return Response(final_response)
-
-
-class GetMostCommonHashtags(APIView):
-    def get(self, request):
-        headers = {
-            'Ocp-Apim-Subscription-Key': settings_local.AZURE['SUBSCRIPTION_KEY'],
-            'Content-type': 'application/json',
-            'X-ClientTraceId': str(uuid.uuid4())
-        }
-
-        tweet = Tweet.objects.all()[0]
-        # You can pass more than one object in body.
-        body = {
-            'documents': [
-                {
-                    'language': 'en',
-                    'id': '1',
-                    'text': tweet.text
-                },
-            ]
-        }
-        response = requests.post(constructed_url, headers=headers, json=body)
-
-        return Response(response.json())
