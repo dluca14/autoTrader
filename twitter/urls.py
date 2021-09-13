ce2d9fca9c20d9d9e92c63439bf430c141750855
divadluca14 <divadluca14@gmail.com>
2021-09-13 19:38:41 +0300

    Change twitter urls

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 import twitter.views as views
 
 urlpatterns = [
-    path('tweets', views.ListTweets.as_view()),
-    path('tweets/most_common', views.GetMostCommonHashtags.as_view()),
-    path('tweets/heat_map', views.GetHeatMap.as_view()),
-    path('tweets/dataframe', views.ListTweetsAsDataFrame.as_view()),
-    path('tweets/stream_tweets/<coin>/', views.StreamTweets.as_view()),
+    path('', views.ListTweets.as_view()),
+    path('most_common', views.GetMostCommonHashtags.as_view()),
+    path('heat_map', views.GetHeatMap.as_view()),
+    path('dataframe', views.ListTweetsAsDataFrame.as_view()),
+    path('stream_tweets/<coin>/', views.StreamTweets.as_view()),
 ]
